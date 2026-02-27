3b714510124ca000b4e89b0c2043b01013340f66
Kendall Castilla <kjcastilla+dev@gmail.com>
2026-02-27 11:27:15 -0500

    Set up routes/ in api/ directory

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,2 +1,16 @@
-import { PrismaClient } from '@prisma/client'
+import * as dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
 
+dotenv.config();
+
+declare global {
+   var __prisma: PrismaClient | undefined;
+}
+
+export const db: PrismaClient = global.__prisma ?? new PrismaClient({
+   log: process.env.NODE_ENV === 'development' ? ['query', 'warn', 'error'] : ['warn', 'error'],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+   global.__prisma = db;
+}
